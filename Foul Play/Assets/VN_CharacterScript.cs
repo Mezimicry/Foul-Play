6ce040150f068e25fc045b31d8ec42d1d0ed1778
JustAPhox <jacobcparrott@gmail.com>
2025-02-20 23:45:23 +0000

    VN First Stage Complete
    VN can now add characters, make them talk, teleport them, and move them.

--- Foul Play/Assets/VN_CharacterScript.cs
+++ Foul Play/Assets/VN_CharacterScript.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class VN_CharacterScript : MonoBehaviour
 {
+    float targetDesination = 25;
+    float targetSpeed = 5;
+    float directionMod = 1;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,20 +18,47 @@ public class VN_CharacterScript : MonoBehaviour
 
     void Update()
     {
+        if (transform.position.x  != targetDesination )
+        {
+            
+            transform.position += new Vector3(targetSpeed, 0, 0) * Time.deltaTime * directionMod ;
 
+            if ((transform.position.x > targetDesination && directionMod == 1) | (transform.position.x < targetDesination && directionMod == -1))
+            {
+
+                transform.position = new Vector3(targetDesination, 0, 0);
+
+
+
+
+            }
+
+
+        }
     }
 
     public void Appear(string sxAxis)
     {
         float xAxis = float.Parse(sxAxis);
         transform.localPosition = new Vector2(xAxis, 0);
+        targetDesination = xAxis;
     }
 
     public void Move(string sXAxis, string sSpeed)
     {
-        float xAxis = float.Parse(sXAxis);
-        float speed = float.Parse(sSpeed);
-        transform.localPosition = new Vector2(xAxis, 0);
+        targetDesination = float.Parse(sXAxis);
+        targetSpeed = float.Parse(sSpeed);
+        if (targetDesination - transform.position.x > 0)
+        {
+            directionMod = 1;
+        }
+        else
+        {
+            directionMod = -1;
+
+        }
+
+        
     }
 
 
