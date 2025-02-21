b207e607ccf7ce6fe4c26363625dcd8ce02d0d32
JustAPhox <jacobcparrott@gmail.com>
2025-02-21 15:22:51 +0000

    Scene Two Complete
    Added left click to advance
    Changed logic script to call a function that will do its action.
    Scene Scripts are now stored in a separate file that can be told which scene to give.
    Text now appears word by word instead of all at once

---
+++ Foul Play/Assets/Scripts/VN_Scripts.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VN_Scripts : MonoBehaviour
+{
+    //Templates
+
+    // , "Say", "Name", "Text"
+    // , "Appear", "Name" , "Location" 
+    // , "Move", "Name" , "Location" , "Speed"
+    // , "Change", "Name" , "SpriteNum" 
+    //
+    //
+
+
+
+    public string[] returnScript(string scriptName)
+    {
+        if (scriptName == "Test")
+        {
+            string[] wantedScript =
+            {
+            "Appear", "Yoomtah", "-5",
+            "Appear", "Kiane", "5",
+            "Say", "Yoomtah", "This is the first text box. After this we will both move.",
+            "Move", "Yoomtah", "5", "3",
+            "Move", "Kiane", "-5", "5",
+            "Say", "Kiane", "Wow how exciting!!!!! \n !!!!!!!!!!",
+            "Say", "Yoomtah", "I will now change spites to be purple",
+            "Change", "Yoomtah", "1",
+            "Say", "Kiane", "Why don't I get a second sprite :'("
+            };
+            return wantedScript;
+            
+        }
+
+
+        string[] errorScript =
+        {
+            "Say", "Phox", "The Script hasn't loaded. Oops"
+        };
+        return errorScript;
+    }
+
+
+
+}
